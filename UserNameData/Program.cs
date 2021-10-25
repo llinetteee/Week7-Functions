647244059ca1814af4bc0b275d347d43a4e86690
Linette Listmann <llistman@tktk.ee>
2021-10-25 14:04:13 +0300

    Control flow

---
+++ UserNameData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UserNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prog palub sisestada kasutaja eesnime
+            //prog kuvab kasutaja eesnime pikkus
+            //prog kuvab kasutaja eesnime esimest tähte
+            //prog kuvab kasutja eesnimetagurpidi
+            //main meetodis ei tohi olla rohkem kui kolm rida koodi
+
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string userName = Console.ReadLine();
+
+            GetUserNameData(userName);
+
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine ($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}.");
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prog palub sisestada kasutaja eesnime
+
+            //prog palub sisestada numbrid 1-3
+
+            //Funktsioonide abil, kolm funktsiooni:
+            //kui kasutaja sisestab '1', siis kuvatakse eesnimi tagurpidi
+            //kui '2', siis kuvatakse eesnime esimest tähte
+            //kui '3', siis kuvatakse kasutaja eesnime pikkus
+
+            Console.WriteLine("Kirjuta oma eesnimi: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Vali number '1','2','3': ");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+        
+
+            switch (userOperator)
+
+            {
+                case '1':
+                    Reverse(userName);
+                    break;
+
+                case '2':
+                    UserNameFirst(userName);
+                    break;
+
+                case '3':
+                    Length(userName);
+                    break;
+                
+                default:
+                    Console.WriteLine("Kena päeva!");
+                    break;
+            }
+        }
+
+        public static void Reverse(string userInput)
+        {
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+
+        public static void UserNameFirst(string userInput)
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}.");
+        }
+
+        public static void Length(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+        }
+    }
+
+
+}
+
+
